8fc998d41a005dfd70eadf4b651a0ea3b52e189d
dynsys-phys-iitm <dynamical.systems.phys.iitm@gmail.com>
2020-09-08 19:15:05 +0530

    header file

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+	<h1>Dynamical Systems Lab</h1>
+	<nav>
+		<ul class="nav">
+			<li><a href = "index.html">About</a></li>
+			<li><a href = "members.html">Lab Members</a></li>
+			<li><a href = "publications.html">Publications</a></li>
+		</ul>
+	</nav>
+	<link href="css/main.css" rel="stylesheet">
+</header>
